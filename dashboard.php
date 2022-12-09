72127a7697ae1aff54165df1aa8de2d9c742428e
= <schultzhenry.boado@obanana.com>
2022-12-09 13:27:35 +0800

    added files

---
+++ dashboard.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DASHBOARD</title>
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
+
+    <!-- SCRIPT -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+
+<body class="bg-primary">
+
+    <div class="container bg-light h-100 mw-75 position-absolute end-0">
+
+        <div class="row border border-2 border-bottom-1 border-top-0 border-start-0 border-end-0 border-secondary mx-5">
+            <p class="h1 text-center m-3">Facility Management System</p>
+        </div>
+
+        <div class="row m-2 gap-2">
+            <div class="col h-25 w-50 bg-primary">
+                <p class="text-center text-light fw-bold mt-2">Total Reservation</p>
+                <p class="text-center text-warning h1">140</p>
+            </div>
+            <div class="col h-25 w-50 bg-primary">
+                <p class="text-center text-light fw-bold mt-2">For Confirmation</p>
+                <p class="text-center text-warning h1">0</p>
+            </div>
+            <div class="col h-25 w-50 bg-primary">
+                <p class="text-center text-light fw-bold mt-2">Cancelled</p>
+                <p class="text-center text-warning h1">3</p>
+            </div>
+        </div>
+
+        <div class="row mt-4">
+            <div class="col text-center">
+                <p class="h1">Schedules Today</p>
+            </div>
+        </div>
+
+    </div>
+
+</body>
+
+</html>
