b4a107519f089e09fe242c242a617375461b01ce
= <schultzhenry.boado@obanana.com>
2022-12-09 16:52:05 +0800

    updated dashboard

--- dashboard.php
+++ dashboard.php
@@ -8,43 +8,34 @@
     <title>DASHBOARD</title>
 
     <!-- CSS -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://getbootstrap.com/docs/5.2/assets/css/docs.css" rel="stylesheet">
 
     <!-- SCRIPT -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
 </head>
 
-<body class="bg-primary">
+<body>
+    <div class="row">
 
-    <div class="container bg-light h-100 mw-75 position-absolute end-0">
-
-        <div class="row border border-2 border-bottom-1 border-top-0 border-start-0 border-end-0 border-secondary mx-5">
-            <p class="h1 text-center m-3">Facility Management System</p>
+        <div class="col-2 text-center bg-primary h-100">
+            <img class="rounded-circle h-25 w-75 mt-2" src="./img/user.jpg" alt="user picture">
+            <p class="h2">Sir Rico</p>
         </div>
 
-        <div class="row m-2 gap-2">
-            <div class="col h-25 w-50 bg-primary">
-                <p class="text-center text-light fw-bold mt-2">Total Reservation</p>
-                <p class="text-center text-warning h1">140</p>
-            </div>
-            <div class="col h-25 w-50 bg-primary">
-                <p class="text-center text-light fw-bold mt-2">For Confirmation</p>
-                <p class="text-center text-warning h1">0</p>
-            </div>
-            <div class="col h-25 w-50 bg-primary">
-                <p class="text-center text-light fw-bold mt-2">Cancelled</p>
-                <p class="text-center text-warning h1">3</p>
-            </div>
-        </div>
 
-        <div class="row mt-4">
-            <div class="col text-center">
-                <p class="h1">Schedules Today</p>
+        <div class="col float-end bg-success text-center ">
+            <p class="h1 m-5">Facility Management System</p>
+
+            <div class="row position-relative text-center bg-danger gap-3">
+                <div class="col h-25 w-75">Total Reservation</div>
+                <div class="col">For Confirmation</div>
+                <div class="col">Cancelled</div>
             </div>
+
         </div>
 
     </div>
-
 </body>
 
 </html>
