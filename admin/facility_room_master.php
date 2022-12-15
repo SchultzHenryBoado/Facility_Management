884a072d810474e5126cf9f172c2835c857a0d22
= <schultzhenry.boado@obanana.com>
2022-12-15 16:57:21 +0800

    added facility room master

---
+++ admin/facility_room_master.php
@@ -0,0 +1,251 @@
+<?php 
+  require_once $_SERVER['DOCUMENT_ROOT'] . '/facility_management/database/connection.php';
+  require './php/session.php';
+
+  // FACILITY ROOM MASTER DATABASE
+  $queryFacilityRoomMaster = "SELECT * FROM facility_room_masters";
+  $sqlFacilityRoomMaster = mysqli_query($con, $queryFacilityRoomMaster);
+
+  // FACILITIES DATABASE
+  $queryFacilities = "SELECT facility_name FROM facilities";
+  $sqlFacilities = mysqli_query($con, $queryFacilities);
+
+  // FLOORS DATABASE
+  $queryFloors = "SELECT floor_name FROM floors";
+  $sqlFloors = mysqli_query($con, $queryFloors);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>FACILITY ROOM MASTER</title>
+
+  <!-- CSS -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous" />
+  <link rel="stylesheet" href="./styles/company.css" />
+
+
+  <!-- JS -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous">
+  </script>
+  <script src="./js/validation.js" defer></script>
+  <!-- FONT AWESOME -->
+  <script src="https://kit.fontawesome.com/8cbc2e0f0e.js" crossorigin="anonymous"></script>
+
+</head>
+
+<body>
+  <!-- NAVBAR -->
+  <nav class="navbar navbar-expand-lg bg-primary">
+    <div class="container-fluid">
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#nav">
+        <i class="fa-sharp fa-solid fa-bars-staggered __icon"></i>
+      </button>
+      <a class="navbar-brand fw-bold text-white __navbar-brand" href="dashboard_admin.php">Facility Management
+        System</a>
+
+      <div class="collapse navbar-collapse" id="nav">
+        <ul class="navbar-nav ms-auto mb-2 mb-lg-0 me-5">
+          <li class="nav-item text-center">
+            <a href="dashboard_admin.php" class="nav-link text-light">Dashboard</a>
+          </li>
+          <li class="nav-item text-center">
+            <a href="#" class="nav-link text-light">Reservation</a>
+          </li>
+          <li class="nav-item text-center">
+            <a href="#" class="nav-link text-light">Inquire</a>
+          </li>
+          <li class="nav-item text-center">
+            <a href="#" class="nav-link text-light">Cancellation</a>
+          </li>
+          <li class="nav-item dropdown text-center">
+            <a class="nav-link dropdown-toggle text-center text-light" href="#" data-bs-toggle="dropdown">
+              Masterfile
+            </a>
+            <ul class="dropdown-menu">
+              <li><a class="dropdown-item" href="register.php">User</a></li>
+              <li><a class="dropdown-item" href="company.php">Company</a></li>
+              <li><a class="dropdown-item" href="floor_master.php">Floor Master</a></li>
+              <li><a class="dropdown-item" href="facility_type.php">Facility Type</a></li>
+              <li><a class="dropdown-item" href="#">Facility Room Master</a></li>
+            </ul>
+          </li>
+
+          <li class="nav-item text-center">
+            <form action="./php/logout.php" method="post">
+              <input type="submit" value="Logout" class="btn btn-primary" />
+            </form>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <!-- FACILITY ROOM MASTER -->
+  <div class="container-fluid mt-5">
+    <div class="container w-50 shadow p-3 mb-5 bg-body rounded">
+      <form action="#" method="post" class="needs-validation" novalidate>
+        <p class="h1 mb-3">Facility Room Master </p>
+
+        <div class="row">
+
+          <!-- FACILITY TYPE -->
+          <div class="col-12 ">
+            <div class="mb-3" class="form-group">
+              <label for="facilityType" class="form-label">Facility Type:</label>
+              <select name="facility_type" id="facilityType" class="form-select" required>
+                <option disabled selected value>-- Facility Type --</option>
+                <?php while($rowFacilities = mysqli_fetch_assoc($sqlFacilities)) { ?>
+                <option value="<?php echo $rowFacilities['facility_name'] ?>">
+                  <?php echo $rowFacilities['facility_name']?></option>
+                <?php } ?>
+              </select>
+              <div class="invalid-feedback">
+                Please choose in the facility type.
+              </div>
+            </div>
+          </div>
+          <!-- FACILITY NUMBER -->
+          <div class="col-12 ">
+            <div class="mb-3" class="form-group">
+              <label for="facilityNumber" class="form-label">Facility Number:</label>
+              <input type="text" name="facility_number" id="facilityNumber" class="form-control" required>
+              <div class="invalid-feedback">
+                Please choose in the facility Number.
+              </div>
+            </div>
+          </div>
+          <!-- DESCRIPTION -->
+          <div class="col-12 ">
+            <div class="mb-3" class="form-group">
+              <label for="description" class="form-label">Description:</label>
+              <input type="text" name="description" id="description" class="form-control" required />
+              <div class="invalid-feedback">
+                Please fill-up the description.
+              </div>
+            </div>
+          </div>
+          <!-- FACILITY NAME -->
+          <div class="col-12 ">
+            <div class="mb-3" class="form-group">
+              <label for="facilityName" class="form-label">Facility Name:</label>
+              <input type="text" name="facility_name" id="facilityName" class="form-control" required>
+              <div class="invalid-feedback">
+                Please fill-up the facility name.
+              </div>
+            </div>
+          </div>
+          <!-- FLOOR LOCATION -->
+          <div class="col-12 ">
+            <div class="mb-3" class="form-group">
+              <label for="floorLocation" class="form-label">Floor Location:</label>
+              <select name="floor_location" id="floorLocation" class="form-select" required>
+                <option disabled selected value>-- Floor Location --</option>
+                <?php while($rowFloorLocation =mysqli_fetch_assoc($sqlFloors)) { ?>
+                <option value="<?php echo $rowFloorLocation['floor_name'] ?>">
+                  <?php echo $rowFloorLocation['floor_name'] ?></option>
+                <?php } ?>
+              </select>
+              <div class="invalid-feedback">
+                Please choose in the floor location.
+              </div>
+            </div>
+          </div>
+          <!-- MAX CAPACITY -->
+          <div class="col-12 ">
+            <div class="mb-3" class="form-group">
+              <label for="maxCapacity" class="form-label">Max Capacity:</label>
+              <input type="text" name="max_capacity" id="maxCapacity" class="form-control" required>
+              <div class="invalid-feedback">
+                Please fill-up the max capacity.
+              </div>
+            </div>
+          </div>
+          <!-- STATUS -->
+          <div class="col-12 ">
+            <div class="mb-3" class="form-group">
+              <label for="status" class="form-label">Status:</label>
+              <select name="status" id="status" class="form-select" required>
+                <option disabled selected value>-- Status --</option>
+                <option value="ACTIVE">ACTIVE</option>
+                <option value="INACTIVE">INACTIVE</option>
+              </select>
+              <div class="invalid-feedback">
+                Please choose in the floor location.
+              </div>
+            </div>
+          </div>
+          <!-- BUTTON -->
+          <div class="col-12">
+            <div class="mb-3">
+              <input type="submit" name="register_facility" value="OK" class="btn btn-success fw-bold float-end" />
+            </div>
+          </div>
+        </div>
+      </form>
+    </div>
+  </div>
+
+  <!-- FACILITY ROOM MASTER LIST -->
+  <div class="container-fluid">
+    <div class="container shadow p-3 mb-5 bg-body-rounded">
+      <div class="table-responsive">
+        <table class="table table-hover table-striped">
+          <thead>
+            <tr>
+              <th scope="col">Facility Type:</th>
+              <th scope="col">Facility Number:</th>
+              <th scope="col">Description:</th>
+              <th scope="col">Facility Name:</th>
+              <th scope="col">Floor Location:</th>
+              <th scope="col">Max Capacity:</th>
+              <th scope="col">Status:</th>
+              <th col="2">Actions:</th>
+            </tr>
+          </thead>
+          <tbody>
+            <?php while($rows = mysqli_fetch_assoc($sqlFacilityRoomMaster)) { ?>
+            <tr>
+              <td><?php echo $rows['facility_type'] ?></td>
+              <td><?php echo $rows['facility_number'] ?></td>
+              <td><?php echo $rows['description'] ?></td>
+              <td><?php echo $rows['facility_name'] ?></td>
+              <td><?php echo $rows['floor_location'] ?></td>
+              <td><?php echo $rows['max_capacity'] ?></td>
+              <td><?php echo $rows['status'] ?></td>
+              <td>
+                <form action="#" method="post">
+                  <input type="submit" name="edit" value="EDIT" class="btn btn-success fw-bold">
+                  <input type="hidden" name="edit_id" value="<?php echo $rows[''] ?>">
+                  <input type="hidden" name="" value="<?php echo $rows[''] ?>">
+                  <input type="hidden" name="" value="<?php echo $rows[''] ?>">
+                  <input type="hidden" name="" value="<?php echo $rows[''] ?>">
+                  <input type="hidden" name="" value="<?php echo $rows[''] ?>">
+                  <input type="hidden" name="" value="<?php echo $rows[''] ?>">
+                  <input type="hidden" name="" value="<?php echo $rows[''] ?>">
+                  <input type="hidden" name="" value="<?php echo $rows[''] ?>">
+                </form>
+              </td>
+              <td>
+                <form action="#" method="post">
+                  <input type="submit" name="delete" class="btn btn-danger fw-bold" value="DELETE">
+                  <input type="hidden" name="delete_id" value="<?php echo $rows[''] ?>">
+                </form>
+              </td>
+            </tr>
+            <?php } ?>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
+</body>
+
+</html>
--- database/facility_management.sql
+++ database/facility_management.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 15, 2022 at 07:29 AM
--- Server version: 10.4.25-MariaDB
--- PHP Version: 7.4.30
+-- Generation Time: Dec 15, 2022 at 09:56 AM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 7.4.33
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -31,7 +31,7 @@ CREATE TABLE `admin_accounts` (
   `id` int(11) NOT NULL,
   `admin_username` varchar(255) NOT NULL,
   `admin_password` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `admin_accounts`
@@ -39,6 +39,87 @@ CREATE TABLE `admin_accounts` (
 
 INSERT INTO `admin_accounts` (`id`, `admin_username`, `admin_password`) VALUES
 (1, 'admin', 'admin123');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `companies`
+--
+
+CREATE TABLE `companies` (
+  `id` int(11) NOT NULL,
+  `company_code` varchar(255) NOT NULL,
+  `company_name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `companies`
+--
+
+INSERT INTO `companies` (`id`, `company_code`, `company_name`) VALUES
+(1, 'OBN', 'Obanana Corp.'),
+(2, 'PMI', 'Premium Megastructures Inc.'),
+(3, 'PIVI', 'Premium Infinite Ventures Inc.');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `facilities`
+--
+
+CREATE TABLE `facilities` (
+  `id` int(11) NOT NULL,
+  `facility_code` varchar(255) NOT NULL,
+  `facility_name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `facilities`
+--
+
+INSERT INTO `facilities` (`id`, `facility_code`, `facility_name`) VALUES
+(1, 'BSKTBLLRM', 'Basketball Room'),
+(4, 'MTNGRM', 'Meeting Room'),
+(5, 'BRDRM', 'Board Room'),
+(6, 'VLLYBLLRM', 'Volleyball Room');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `facility_room_masters`
+--
+
+CREATE TABLE `facility_room_masters` (
+  `id` int(11) NOT NULL,
+  `facility_type` varchar(255) NOT NULL,
+  `facility_number` varchar(255) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `facility_name` varchar(255) NOT NULL,
+  `floor_location` varchar(255) NOT NULL,
+  `max_capacity` varchar(255) NOT NULL,
+  `status` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `floors`
+--
+
+CREATE TABLE `floors` (
+  `id` int(11) NOT NULL,
+  `floor_code` varchar(255) NOT NULL,
+  `floor_name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `floors`
+--
+
+INSERT INTO `floors` (`id`, `floor_code`, `floor_name`) VALUES
+(1, '15F', '15th floor'),
+(3, '16F', '16th floor'),
+(4, '17F', '17th floor');
 
 -- --------------------------------------------------------
 
@@ -51,17 +132,21 @@ CREATE TABLE `users_accounts` (
   `last_names` varchar(255) NOT NULL,
   `first_names` varchar(255) NOT NULL,
   `username` varchar(255) NOT NULL,
+  `company` varchar(255) NOT NULL,
   `emails` varchar(255) NOT NULL,
   `passwords` varchar(255) NOT NULL,
-  `status` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `statuses` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `users_accounts`
 --
 
-INSERT INTO `users_accounts` (`id`, `last_names`, `first_names`, `username`, `emails`, `passwords`, `status`) VALUES
-(4, 'Boado', 'Schultz Henry', 'henryboado', 'schultzhenry.boado@obanana.com', 'e99a18c428cb38d5f260853678922e03', 'ACTIVE');
+INSERT INTO `users_accounts` (`id`, `last_names`, `first_names`, `username`, `company`, `emails`, `passwords`, `statuses`) VALUES
+(1, 'Boado', 'Schultz Henry', 'henryboado', 'Obanana Corp.', 'schultzhenry.boado@obanana.com', '202cb962ac59075b964b07152d234b70', 'ACTIVE'),
+(2, 'Gloda', 'John Bryan', 'bryangloda', 'Premium Megastructures Inc.', 'bryan.gloda@obanana.com', 'e99a18c428cb38d5f260853678922e03', 'ACTIVE'),
+(3, 'Mangalo', 'Ryan Christian', 'ryanmangalo', 'Obanana Corp.', 'ryan.mangalo@obanana.com', 'e99a18c428cb38d5f260853678922e03', 'ACTIVE'),
+(8, 'Matias', 'Ryan', 'ryanmatias', 'Obanana Corp.', 'ryan.matias@obanana.com', '57f231b1ec41dc6641270cb09a56f897', 'INACTIVE');
 
 --
 -- Indexes for dumped tables
@@ -71,6 +156,30 @@ INSERT INTO `users_accounts` (`id`, `last_names`, `first_names`, `username`, `em
 -- Indexes for table `admin_accounts`
 --
 ALTER TABLE `admin_accounts`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `companies`
+--
+ALTER TABLE `companies`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `facilities`
+--
+ALTER TABLE `facilities`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `facility_room_masters`
+--
+ALTER TABLE `facility_room_masters`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `floors`
+--
+ALTER TABLE `floors`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -90,10 +199,34 @@ ALTER TABLE `admin_accounts`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
+-- AUTO_INCREMENT for table `companies`
+--
+ALTER TABLE `companies`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `facilities`
+--
+ALTER TABLE `facilities`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `facility_room_masters`
+--
+ALTER TABLE `facility_room_masters`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `floors`
+--
+ALTER TABLE `floors`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
 -- AUTO_INCREMENT for table `users_accounts`
 --
 ALTER TABLE `users_accounts`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
