755ddad5b110789a9c702ad819e4982d3dedf0e5
= <schultzhenry.boado@obanana.com>
2023-01-06 15:08:25 +0800

    updated

--- admin/php/reservation_reject.php
+++ admin/php/reservation_reject.php
@@ -11,7 +11,7 @@
     $reject = $_POST['reject_id'];
     $rejectReasons = filter_input(INPUT_POST, 'reject_reasons', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
 
-    $sqlRejectReservation = "UPDATE reservations SET statuses = 'REJECT', cancel_reasons = '$rejectReasons' WHERE id = '$reject'";
+    $sqlRejectReservation = "UPDATE reservations SET statuses = 'REJECT', cancel_reasons=? WHERE id=?";
     $stmt = $con->prepare($sqlRejectReservation);
     $stmt->execute([$rejectReasons, $reject]);
 
