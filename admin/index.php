93e59b30645cc5f352ddf8c231caa0688bff6653
= <schultzhenry.boado@obanana.com>
2023-01-06 15:28:26 +0800

    updated

--- admin/index.php
+++ admin/index.php
@@ -1,6 +1,6 @@
 <?php 
 
-  // include_once $_SERVER['DOCUMENT_ROOT'] . '/facility_management/database/connection.php'; 
+  include_once $_SERVER['DOCUMENT_ROOT'] . '/facility_management/database/connection.php'; 
 
   session_start();
 
