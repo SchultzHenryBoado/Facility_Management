a713d04c90fe8b88412f39c2bd3af745bf4aa8e0
= <schultzhenry.boado@obanana.com>
2023-01-11 08:27:28 +0800

    updated

--- employee/cancellation.php
+++ employee/cancellation.php
@@ -10,7 +10,27 @@
   $stmtCancel = $con->prepare($sqlReservationCancel);
   $stmtCancel->execute([$users_id]);
 
+  // RESERVATIONS
+  $sqlReadReservations = "SELECT * FROM reservations";
+  $stmtReservations = $con->prepare($sqlReadReservations);
+  $stmtReservations->execute();
 
+  if (isset($_POST['update'])) {
+    $updateId = $_POST['update_id'];
+    $updateRsvnNo = filter_input(INPUT_POST, 'update_rsvn_no', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+    $updateRoomType = filter_input(INPUT_POST, 'update_room_type', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+    $updateDateFrom = filter_input(INPUT_POST, 'update_date_from', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+    $updateDateTo = filter_input(INPUT_POST, 'update_date_to', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+    $updateTimeFrom = filter_input(INPUT_POST, 'update_time_from', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+    $updateTimeTo = filter_input(INPUT_POST, 'update_time_to', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+    $updateStatus = filter_input(INPUT_POST, 'update_status', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+
+    $sqlUpdate = "UPDATE reservations SET rsvn_no=?, room_type=?, date_from=?, date_to=?, time_from=?, time_to=?, statuses=? WHERE id=?";
+    $stmtUpdate = $con->prepare($sqlUpdate);
+    $stmtUpdate->execute([$updateRsvnNo, $updateRoomType, $updateDateFrom, $updateDateTo, $updateTimeFrom, $updateTimeTo, $updateStatus, $updateId]);
+
+    path('reservation');
+  }
 ?>
 
 <!DOCTYPE html>
@@ -32,6 +52,8 @@
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
     integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous">
   </script>
+  <script src="./js/date.js" defer></script>
+  <script src="./js/validation.js" defer></script>
 
   <!-- FONT AWESOME -->
   <script src="https://kit.fontawesome.com/8cbc2e0f0e.js" crossorigin="anonymous"></script>
@@ -111,76 +133,108 @@
             <td><?php echo $rowReserveCancel->statuses ?></td>
             <td><?php echo $rowReserveCancel->cancel_reasons ?></td>
             <td>
-              <button type="button" class="btn btn-warning" data-bs-toggle="modal" data-bs-target="#editModal">
+              <button type="button" class="btn btn-warning" data-bs-toggle="modal"
+                data-bs-target="#editModal-<?php echo $rowReserveCancel->id ?>">
                 <i class="fa-solid fa-pen-to-square"></i>
               </button>
 
-              <!-- Modal -->
-              <div class="modal fade" id="editModal" tabindex="-1">
-                <div class="modal-dialog modal-dialog-centered">
-                  <div class="modal-content">
-                    <div class="modal-header">
-                      <h1 class="modal-title fs-5" id="exampleModalLabel">Update Reservation</h1>
-                    </div>
-                    <div class="modal-body">
-                      <div class="row">
-                        <div class="col-12">
-                          <div class="mb-3">
-                            <label for="rsvn_no" class="form-label">RSVN No.</label>
-                            <input type="text" name="update_rsvn_no" id="rsvn_no" class="form-control">
+              <form action="#" method="post" class="needs-validation" novalidate>
+                <?php 
+                $sqlRoom = "SELECT * FROM facilities";
+                $stmtRoom = $con->prepare($sqlRoom);
+                $stmtRoom->execute();
+              ?>
+
+                <!-- Modal -->
+                <div class="modal fade" id="editModal-<?php echo $rowReserveCancel->id ?>" tabindex="-1">
+                  <input type="hidden" name="update_id" value="<?php echo $rowReserveCancel->id ?>">
+                  <div class="modal-dialog modal-dialog-centered">
+                    <div class="modal-content">
+                      <div class="modal-header">
+                        <h1 class="modal-title fs-5" id="exampleModalLabel">Update Reservation</h1>
+                      </div>
+                      <div class="modal-body">
+                        <div class="row">
+                          <div class="col-12">
+                            <div class="mb-3">
+                              <label for="rsvn_no" class="form-label">RSVN No.</label>
+                              <input type="text" name="update_rsvn_no" id="rsvn_no" class="form-control"
+                                value="<?php echo $rowReserveCancel->rsvn_no ?>" required>
+                              <div class="invalid-feedback">
+                                Please input an rsvn no.
+                              </div>
+                            </div>
                           </div>
-                        </div>
-                        <div class="col-12">
-                          <div class="mb-3">
-                            <label for="createdBy" class="form-label">Created By</label>
-                            <input type="text" name="update_created_by" id="createdBy" class="form-control">
+                          <div class="col-12">
+                            <div class="mb-3">
+                              <label for="roomType" class="form-label">Room Type</label>
+                              <select name="update_room_type" id="roomType" class="form-select" required>
+                                <option disabled selected value>--Choose a room type</option>
+                                <?php while($rowRoomType = $stmtRoom->fetch()) { ?>
+                                <option value="<?php echo $rowRoomType->facility_name ?>">
+                                  <?php echo $rowRoomType->facility_name ?></option>
+                                <?php } ?>
+                              </select>
+                              <div class="invalid-feedback">
+                                Please choose in room type.
+                              </div>
+                            </div>
                           </div>
-                        </div>
-                        <div class="col-12">
-                          <div class="mb-3">
-                            <label for="roomType" class="form-label">Room Type</label>
-                            <select name="update_room_type" id="roomType" class="form-select"></select>
+                          <div class="col-12">
+                            <div class="mb-3">
+                              <label for="updateDateFrom" class="form-label">Date from</label>
+                              <input type="date" name="update_date_from" id="updateDateFrom" class="form-control"
+                                value="<?php echo $rowReserveCancel->date_from ?>" required>
+                              <div class="invalid-feedback">
+                                Please input a date from.
+                              </div>
+                            </div>
                           </div>
-                        </div>
-                        <div class="col-12">
-                          <div class="mb-3">
-                            <label for="dateFrom" class="form-label">Date from</label>
-                            <input type="date" name="update_date_from" id="dateFrom" class="form-control">
+                          <div class="col-12">
+                            <div class="mb-3">
+                              <label for="updateDateTo" class="form-label">Date To</label>
+                              <input type="date" name="update_date_to" id="updateDateTo" class="form-control"
+                                value="<?php echo $rowReserveCancel->date_to ?>" required>
+                              <div class="invalid-feedback">
+                                Please input a date to.
+                              </div>
+                            </div>
                           </div>
-                        </div>
-                        <div class="col-12">
-                          <div class="mb-3">
-                            <label for="dateTo" class="form-label">Date To</label>
-                            <input type="date" name="update_date_to" id="dateTo" class="form-control">
+                          <div class="col-12">
+                            <div class="mb-3">
+                              <label for="updateTimeFrom" class="form-label">Time From</label>
+                              <input type="time" name="update_time_from" id="updateTimeFrom" class="form-control"
+                                value="<?php echo $rowReserveCancel->time_from ?>" required>
+                              <div class="invalid-feedback">
+                                Please input a time from.
+                              </div>
+                            </div>
                           </div>
-                        </div>
-                        <div class="col-12">
-                          <div class="mb-3">
-                            <label for="timeFrom" class="form-label">Time From</label>
-                            <input type="time" name="update_time_from" id="timeFrom" class="form-control">
+                          <div class="col-12">
+                            <div class="mb-3">
+                              <label for="updateTimeTo" class="form-label">Time To</label>
+                              <input type="time" name="update_time_to" id="updateTimeTo" class="form-control"
+                                value="<?php echo $rowReserveCancel->time_to ?>" required>
+                              <div class="invalid-feedback">
+                                Please input a time to.
+                              </div>
+                            </div>
                           </div>
-                        </div>
-                        <div class="col-12">
-                          <div class="mb-3">
-                            <label for="timeTo" class="form-label">Time To</label>
-                            <input type="time" name="update_time_to" id="timeTo" class="form-control">
+                          <div class="col-12">
+                            <label for="status" class="form-label">Status</label>
+                            <input type="text" name="update_status" class="form-control" id="status" value="PENDING"
+                              readonly>
                           </div>
-                        </div>
-                        <div class="col-12">
-                          <label for="status" class="form-label">Status</label>
-                          <select name="update_status" id="status" class="form-select">
-                            <option></option>
-                          </select>
                         </div>
                       </div>
-                    </div>
-                    <div class="modal-footer">
-                      <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                      <button type="button" class="btn btn-success">Edit</button>
+                      <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary fw-bold" data-bs-dismiss="modal">Close</button>
+                        <button type="submit" class="btn btn-success fw-bold" name="update">Update</button>
+                      </div>
                     </div>
                   </div>
                 </div>
-              </div>
+              </form>
             </td>
           </tr>
           <?php } ?>
