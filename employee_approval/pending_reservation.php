517a01bbddd54b851fed5038967f446478dc13e4
unknown <schultzhenry.boado@obanana.com>
2023-01-12 21:31:49 -0800

    updated the users_create

--- employee_approval/pending_reservation.php
+++ employee_approval/pending_reservation.php
@@ -23,7 +23,6 @@ $stmt->execute();
   <!-- CSS -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous" />
-  <link rel="stylesheet" href="./styles/reserve.css" />
 
 
   <!-- JS -->
