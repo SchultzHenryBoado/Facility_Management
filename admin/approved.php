8847772bcfae82576466de84f61893edc85c1be2
= <schultzxhenry@gmail.com>
2023-01-21 16:59:58 -0800

    updated

--- admin/approved.php
+++ admin/approved.php
@@ -23,13 +23,18 @@ $stmt->execute();
   <!-- CSS -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous" />
+  <link rel="stylesheet" href="//cdn.datatables.net/1.13.1/css/jquery.dataTables.min.css">
 
   <link rel="stylesheet" href="./styles/cancellation.css" />
 
   <!-- JS -->
+  <script src="https://code.jquery.com/jquery-3.6.3.min.js"
+    integrity="sha256-pvPw+upLPUjgMXY0G+8O0xUf+/Im1MZjXxxgOcBQBXU=" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
     integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous">
   </script>
+  <script src="//cdn.datatables.net/1.13.1/js/jquery.dataTables.min.js" defer></script>
+  <script src="./js/dataTable.js" defer></script>
 
   <!-- FONT AWESOME -->
   <script src="https://kit.fontawesome.com/8cbc2e0f0e.js" crossorigin="anonymous"></script>
@@ -97,17 +102,17 @@ $stmt->execute();
   <div class="container-fluid mt-5">
     <div class="container shadow p-3 mb-5 bg-body-rounded">
       <div class="table-responsive">
-        <table class="table table-striped table-hover">
-          <thead class="bg-primary text-white">
+        <table class="table table-striped table-hover" id="myTable">
+          <thead>
             <tr>
-              <th scope="col">RSVN No.</th>
-              <th scope="col">Room Type:</th>
-              <th scope="col">Date From:</th>
-              <th scope="col">Time From:</th>
-              <th scope="col">Time To:</th>
-              <th scope="col">Reserved By:</th>
-              <th scope="col">Status:</th>
-              <th scope="col">Action:</th>
+              <th>RSVN No.</th>
+              <th>Room Type:</th>
+              <th>Date From:</th>
+              <th>Time From:</th>
+              <th>Time To:</th>
+              <th>Reserved By:</th>
+              <th>Status:</th>
+              <th>Action:</th>
             </tr>
           </thead>
           <tbody>
